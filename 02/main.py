bf1094a53b46e71bb19f294953e7c253ffc6a591
David Fisco <dave@davidfisco.com>
2023-12-02 16:12:03 -0500

    Ported all of Day 2 to Python.

--- 02/main.py
+++ 02/main.py
@@ -1,28 +1,19 @@
-import re
-
-text_to_day = {
-      "one": "o1e", 
-      "two": "t2o",
-      "three": "th3ee",
-      "four": "fo4ur", 
-      "five": "fi5ve", 
-      "six": "s6x",
-      "seven": "se7en", 
-      "eight": "ei8ht", 
-      "nine": "ni9ne"}
-
-calibration = 0
+total = 0
+game_power_total = 0
 
 with open('input.txt', 'r') as f:
     for line in f:
-        for key in text_to_day:
-            pattern = re.compile(key)
-            line = re.sub(pattern, text_to_day[key], line)
-        line = re.sub(r"[^\d]", "", line)
-        if len(line) == 1:
-            line = line + line
-        if len(line) > 2:
-            line = line[0] + line[-1]
-        calibration += int(line)
-
-print(calibration)
+        cube_maxes = { "red": 0, "green": 0, "blue": 0 }
+        game_power = 1
+        for game in line.split(": ")[1].strip().split("; "):
+            for showing in game.split(", "):
+                if int(showing.strip().split(" ")[0]) > cube_maxes[showing.strip().split(" ")[1]]:
+                    cube_maxes[showing.strip().split(" ")[1]] = int(showing.strip().split(" ")[0])
+        if cube_maxes["red"] <= 12 and cube_maxes["green"] <= 13 and cube_maxes["blue"] <= 14:
+            total += int(line.split(": ")[0].strip().split(" ")[1])
+        for key in cube_maxes:
+            game_power *= cube_maxes[key]
+        game_power_total += game_power
+                     
+print("The total is: " + str(total))
+print("The game power total is: " + str(game_power_total))
